c1a2bf0761ac2935abc4295873be3489321e10c4
Vu Pham <vu.pham@zingbox.com>
2020-07-22 11:44:18 -0700

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+export {
+  MyButton
+}
+from './src/elements/button';
---
+++ src/elements/button.ts
@@ -0,0 +1,31 @@
+import {LitElement, html, customElement, property, css} from 'lit-element';
+
+@customElement('my-button')
+export class MyButton extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+      border: solid 1px gray;
+      padding: 16px;
+      max-width: 800px;
+    }
+  `;
+
+  /**
+   * The number of times the button has been clicked.
+   */
+  @property({type: Number})
+  count = 0;
+
+  render() {
+    return html`
+      <button @click=${this._onClick} part="button">
+        Click Count: ${this.count}
+      </button>
+    `;
+  }
+
+  private _onClick() {
+    this.count++;
+  }
+}
